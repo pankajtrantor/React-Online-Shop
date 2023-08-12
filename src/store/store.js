a2d4b1b5b15f8e38e5cc5dff3b6c91cda8dc02b4
Pankaj <Pankaj.sharma@trantorinc.com>
2023-08-12 13:14:53 +0530

    Add files via upload

--- src/store/store.js
+++ src/store/store.js
@@ -1,1 +1,16 @@
+import React from 'react';
+import {applyMiddleware, compose, createStore} from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import rootReducer from './reducers/index';
+import thunk from "redux-thunk";
+const persistConfig = {
+    key: 'root',
+    storage: storage,
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
+let store = createStore(persistedReducer,composeEnhancers(applyMiddleware(thunk)))
+let persistor = persistStore(store)
+export  { store, persistor }
